7a4f986e03eb878fc0741be7308fc44063988f9f
Jeoffrey Thirot <jeoffrey.thirot@gmail.com>
2024-04-24 07:08:41 +0200

    Initial Commit

---
+++ RepoProtoTest/RepoProtoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoProtoTestApp.swift
+//  RepoProtoTest
+//
+//  Created by Jeoffrey Thirot on 24/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RepoProtoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
